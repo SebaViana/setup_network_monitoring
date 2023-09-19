e978e75159b4b8a6d5014a82f5177839e41790f1
Sebastián Viana <vianasebastian2000@gmail.com>
2023-09-19 12:53:01 -0300

    Implemented functional speedtest exporter

--- setup.sh
+++ setup.sh
@@ -2,55 +2,88 @@
 
 # Get script absolute path
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-DATA_DIR="$SCRIPT_DIR/data"
+REPOS_DIR="$SCRIPT_DIR/data/repos"
 
-# Clone or update repo
-REPO_NAME="icmp_ping_exporter"
-IMAGE_TAG="latest"
+# Clone or update icmp_ping_exporter repo
+ICMP_REPO_NAME="icmp_ping_exporter"
+ICMP_IMAGE_TAG="latest"
 
-REPO_URL="https://github.com/SebaViana/icmp_ping_exporter.git"
-RELEASE_VER="v1.0.0"
+ICMP_REPO_URL="https://github.com/SebaViana/icmp_ping_exporter.git"
+ICMP_RELEASE_VER="v1.0.0"
 
-cd "$DATA_DIR" || exit
-if [ -d "$REPO_NAME" ]; then
-    echo "Repository directory '$REPO_NAME' already exists."
-    cd "$REPO_NAME" || exit
+mkdir -p "$REPOS_DIR"  # Ensure the repos directory exists
+
+cd "$REPOS_DIR" || exit
+if [ -d "$ICMP_REPO_NAME" ]; then
+    echo "Repository directory '$ICMP_REPO_NAME' already exists."
+    cd "$ICMP_REPO_NAME" || exit
     CURRENT_TAG="$(git describe --tags)"
-    if [ "$CURRENT_TAG" = "$RELEASE_VER" ]; then
-        echo "Repository is already at release version '$RELEASE_VER'."
+    if [ "$CURRENT_TAG" = "$ICMP_RELEASE_VER" ]; then
+        echo "Repository is already at release version '$ICMP_RELEASE_VER'."
     else
-        echo "Updating repository to release version '$RELEASE_VER'..."
+        echo "Updating repository '$ICMP_REPO_NAME' to release version '$ICMP_RELEASE_VER'..."
         git fetch --tags
-        git checkout "$RELEASE_VER"
+        git checkout "$ICMP_RELEASE_VER"
         if [ $? -eq 0 ]; then
-            echo "Repository updated to release version '$RELEASE_VER' successfully."
+            echo "Repository updated to release version '$ICMP_RELEASE_VER' successfully."
         else
-            echo "Failed to update the repository to release version '$RELEASE_VER'."
+            echo "Failed to update the repository to release version '$ICMP_RELEASE_VER'."
             exit 1
         fi
     fi
 else
-    git clone -b "$RELEASE_VER" "$REPO_URL" "$REPO_NAME"
+    git clone -b "$ICMP_RELEASE_VER" "$ICMP_REPO_URL" "$ICMP_REPO_NAME"
     if [ $? -eq 0 ]; then
-        echo "Repository cloned successfully."
+        echo "Repository '$ICMP_REPO_NAME' cloned successfully."
     else
-        echo "Failed to clone the repository."
+        echo "Failed to clone the repository '$ICMP_REPO_NAME'."
         exit 1
     fi
 fi
 
-# Build Docker image
-cd "$DATA_DIR/$REPO_NAME" || exit
+# Build Docker image for icmp_ping_exporter
+cd "$REPOS_DIR/$ICMP_REPO_NAME" || exit
 
-docker build -t "$REPO_NAME:$IMAGE_TAG" .
+docker build -t "$ICMP_REPO_NAME:$ICMP_IMAGE_TAG" .
 
 if [ $? -eq 0 ]; then
-    echo "Docker image '$REPO_NAME:$IMAGE_TAG' built successfully."
+    echo "Docker image '$ICMP_REPO_NAME:$ICMP_IMAGE_TAG' built successfully."
 else
-    echo "Failed to build the Docker image."
+    echo "Failed to build the Docker image for '$ICMP_REPO_NAME'."
+    exit 1
+fi
+
+# Clone or update speedtest_exporter repo
+SPEEDTEST_REPO_NAME="speedtest_exporter"
+SPEEDTEST_IMAGE_TAG="latest"
+
+SPEEDTEST_REPO_URL="https://github.com/SebaViana/speedtest_exporter.git"
+
+if [ -d "$SPEEDTEST_REPO_NAME" ]; then
+    echo "Repository directory '$SPEEDTEST_REPO_NAME' already exists."
+    cd "$REPOS_DIR" || exit
+else
+    git clone "$SPEEDTEST_REPO_URL" "$REPOS_DIR/$SPEEDTEST_REPO_NAME"
+    if [ $? -eq 0 ]; then
+        echo "Repository '$SPEEDTEST_REPO_NAME' cloned successfully."
+    else
+        echo "Failed to clone the repository '$SPEEDTEST_REPO_NAME'."
+        exit 1
+    fi
+fi
+
+# Build Docker image for speedtest_exporter
+cd "$REPOS_DIR/$SPEEDTEST_REPO_NAME" || exit
+
+docker build -t "$SPEEDTEST_REPO_NAME:$SPEEDTEST_IMAGE_TAG" .
+
+if [ $? -eq 0 ]; then
+    echo "Docker image '$SPEEDTEST_REPO_NAME:$SPEEDTEST_IMAGE_TAG' built successfully."
+else
+    echo "Failed to build the Docker image for '$SPEEDTEST_REPO_NAME'."
     exit 1
 fi
 
 # Run Ansible playbook, it runs also Docker Compose
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR"
 ansible-playbook main.yml
